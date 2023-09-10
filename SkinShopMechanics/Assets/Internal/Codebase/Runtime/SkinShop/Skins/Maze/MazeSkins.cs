1848d5b3f2d6781d3adf519e85e6c8e61144be23
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 20:13:15 +0400

    Create MazeSkins

---
+++ SkinShopMechanics/Assets/Internal/Codebase/Runtime/SkinShop/Skins/Maze/MazeSkins.cs
@@ -0,0 +1,24 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+namespace RimuruDev.Internal.Codebase.Runtime.SkinShop.Skins.Maze
+{
+    public enum MazeSkins
+    {
+        Green = 0,
+        Egypt = 1,
+        Cristall = 2,
+        Jungle = 3,
+        China = 4,
+        Beach = 5,
+        Treasury = 6,
+    }
+}
