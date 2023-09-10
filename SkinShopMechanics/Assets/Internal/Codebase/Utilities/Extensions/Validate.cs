b0c041a13d653f4a484372f9bfd1bf54549183e2
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 22:46:07 +0400

    Create Validate

---
+++ SkinShopMechanics/Assets/Internal/Codebase/Utilities/Extensions/Validate.cs
@@ -0,0 +1,19 @@
+﻿// Resharper disable All
+
+using System;
+using System.Linq;
+
+public static class Validate
+{
+    public static void Null(params object[] objs)
+    {
+        if (objs.Any(obj => obj == null))
+            throw new NullReferenceException(nameof(objs));
+    }
+
+    public static void CheckNull(this object obj)
+    {
+        if (obj == null)
+            throw new NullReferenceException(nameof(obj));
+    }
+}
