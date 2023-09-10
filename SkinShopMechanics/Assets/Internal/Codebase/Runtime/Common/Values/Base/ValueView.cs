d85dbaad31afd6a833a8576f73674c6cbc02d0be
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 22:06:02 +0400

    Create ValueView and Implementations for ValueView

---
+++ SkinShopMechanics/Assets/Internal/Codebase/Runtime/Common/Values/Base/ValueView.cs
@@ -0,0 +1,35 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace RimuruDev.Internal.Codebase.Runtime.Common.Values.Base
+{
+    [DisallowMultipleComponent]
+    public class ValueView<TValue> : MonoBehaviour where TValue : IConvertible
+    {
+        [SerializeField] private TMP_Text valueText;
+
+        public void Show(TValue value)
+        {
+            if (valueText == null)
+                throw new NullReferenceException(nameof(valueText));
+
+            gameObject.SetActive(true);
+            valueText.text = $"{value}";
+        }
+
+        public void Hide() =>
+            gameObject.SetActive(true);
+    }
+}
---
+++ SkinShopMechanics/Assets/Internal/Codebase/Runtime/Common/Values/Implementations/DoubleValueView.cs
@@ -0,0 +1,19 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+using RimuruDev.Internal.Codebase.Runtime.Common.Values.Base;
+
+namespace RimuruDev.Internal.Codebase.Runtime.Common.Values.Implementations
+{
+    public sealed class DoubleValueView : ValueView<double>
+    {
+    }
+}
