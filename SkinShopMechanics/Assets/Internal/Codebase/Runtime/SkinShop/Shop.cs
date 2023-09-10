f2f3f257ba344470d67f3380f10cb673810b20f6
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 23:50:20 +0400

    Create Shop

---
+++ SkinShopMechanics/Assets/Internal/Codebase/Runtime/SkinShop/Shop.cs
@@ -0,0 +1,29 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+using UnityEngine;
+using NaughtyAttributes;
+using RimuruDev.Internal.Codebase.Runtime.SkinShop.Panels;
+using RimuruDev.Internal.Codebase.Runtime.SkinShop.Skins.Configs;
+
+namespace RimuruDev.Internal.Codebase.Runtime.SkinShop
+{
+    [DisallowMultipleComponent]
+    public sealed class Shop : MonoBehaviour
+    {
+        [SerializeField, Expandable] private ShopContent contentItems;
+
+        [SerializeField] private ShopCategoryButton characterSkinButtons;
+        [SerializeField] private ShopCategoryButton mazeSkinButtons;
+
+        [SerializeField] private ShopPanel shopPanel;
+    }
+}
