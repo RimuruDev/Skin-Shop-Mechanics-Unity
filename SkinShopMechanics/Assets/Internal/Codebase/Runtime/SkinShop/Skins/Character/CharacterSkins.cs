b86460a6cca9570de757c14c8b19d0197a9f17c3
Rimuru <Rimuru.Dev@gmail.com>
2023-09-10 20:12:54 +0400

    Create CharacterSkins

---
+++ SkinShopMechanics/Assets/Internal/Codebase/Runtime/SkinShop/Skins/Character/CharacterSkins.cs
@@ -0,0 +1,36 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+namespace RimuruDev.Internal.Codebase.Runtime.SkinShop.Skins.Character
+{
+    public enum CharacterSkins
+    {
+        Anubis = 0,
+        Bat = 1,
+        Bear = 2,
+        BearArmor = 3,
+        Cat = 4,
+        Deer = 5,
+        Dog = 6,
+        DogBeach = 7,
+        Dragon = 8,
+        Hare = 9,
+        Monkey = 10,
+        Mouse = 11,
+        Panda = 12,
+        Parrot = 13,
+        Penguin = 14,
+        Pharaon = 15,
+        Ra = 16,
+        RedPanda = 17,
+        Duck = 18,
+    }
+}
