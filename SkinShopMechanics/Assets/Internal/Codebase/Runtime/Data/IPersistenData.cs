f6cc84f3f90e1eb998e05bda4d3b102eb1aa45eb
Rimuru <Rimuru.Dev@gmail.com>
2023-09-11 00:48:53 +0400

    Create IPersistenData

---
+++ SkinShopMechanics/Assets/Internal/Codebase/Runtime/Data/IPersistenData.cs
@@ -0,0 +1,18 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+namespace RimuruDev.Internal.Codebase.Runtime.Data
+{
+    public interface IPersistenData
+    {
+        public PlayerData PlayerData { get; set; }
+    }
+}
