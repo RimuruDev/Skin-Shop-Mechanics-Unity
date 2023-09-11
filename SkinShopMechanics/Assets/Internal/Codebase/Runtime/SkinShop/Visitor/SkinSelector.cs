7040154f8b2994167f22096a606422d38ed50060
Rimuru <Rimuru.Dev@gmail.com>
2023-09-12 00:22:35 +0400

    Create SkinSelector

---
+++ SkinShopMechanics/Assets/Internal/Codebase/Runtime/SkinShop/Visitor/SkinSelector.cs
@@ -0,0 +1,36 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+using System;
+using RimuruDev.Internal.Codebase.Infrastructura.Services.PersistenProgress;
+using RimuruDev.Internal.Codebase.Runtime.SkinShop.Skins.Character;
+using RimuruDev.Internal.Codebase.Runtime.SkinShop.Skins.Configs;
+using RimuruDev.Internal.Codebase.Runtime.SkinShop.Skins.Maze;
+
+namespace RimuruDev.Internal.Codebase.Runtime.SkinShop.Visitor
+{
+    public sealed class SkinSelector : IShopItemVisitor
+    {
+        private readonly IPersistenData persistenData;
+
+        public SkinSelector(IPersistenData persistenData) =>
+            this.persistenData = persistenData ?? throw new ArgumentNullException(nameof(persistenData));
+
+        public void Visit(ShopItem shopItem) =>
+            this.Visit((dynamic)shopItem);
+
+        public void Visit(CharacterSkinItem characterSkinItem) =>
+            persistenData.PlayerData.SelectCharacterSkins = characterSkinItem.SkinType;
+
+        public void Visit(MazeSkinItem mazeSkinItem) =>
+            persistenData.PlayerData.SelectMazeSkins = mazeSkinItem.SkinType;
+    }
+}
